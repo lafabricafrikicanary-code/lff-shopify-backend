5b6735d4a520d7358b570fa5af45be178760a410
Alejandro Cabrera <otakucanary7@gmail.com>
2026-09-25 22:17:04 +0100

    Fallback app URL for Render

--- app/shopify.server.js
+++ app/shopify.server.js
@@ -7,12 +7,17 @@ import {
 import { PrismaSessionStorage } from "@shopify/shopify-app-session-storage-prisma";
 import prisma from "./db.server";
 
+const appUrl =
+  process.env.SHOPIFY_APP_URL ||
+  process.env.RENDER_EXTERNAL_URL ||
+  "https://lff-shopify-backend.onrender.com";
+
 const shopify = shopifyApp({
   apiKey: process.env.SHOPIFY_API_KEY,
   apiSecretKey: process.env.SHOPIFY_API_SECRET || "",
   apiVersion: ApiVersion.July26,
   scopes: process.env.SCOPES?.split(","),
-  appUrl: process.env.SHOPIFY_APP_URL || "",
+  appUrl,
   authPathPrefix: "/auth",
   sessionStorage: new PrismaSessionStorage(prisma),
   distribution: AppDistribution.AppStore,
